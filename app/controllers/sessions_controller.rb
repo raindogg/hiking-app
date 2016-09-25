671afaef5fe99a423bdd9d2ba7847228a991003c
wabray17 <wabray17@gmail.com>
2016-09-25 16:55:42 -0500

    Login, logout, signup functions added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,27 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in. Welcome back #{user.name}"
+      redirect_to '/'
+    else
+      flash[:warning] = "Invalid username or password. Try again."
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+    if session[:user_id]
+      session[:user_id] = nil
+      flash[:success] = "Successfully logged out."
+      redirect_to '/login'
+    else
+      flash[:warning] = "You must sign in before you can sign out."
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'treks#index'
-  
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
 end
