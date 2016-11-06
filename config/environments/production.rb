0445661d9131d5c2ddad0ab360a1d6d7baaadc5e
wabray17 <wabray17@gmail.com>
2016-11-06 15:48:44 -0600

    minifier?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,5 +96,7 @@ Rails.application.configure do
   }
 
   # Do not dump schema after migrations.
+
+  config.assets.js_compressor = Uglifier.new(mangle: false)
   config.active_record.dump_schema_after_migration = false
 end
